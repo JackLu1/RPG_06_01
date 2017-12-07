3dc40ea04fca3a5f4cb158ef024a8594abb78210
Jack Lu <jlu2025@gmail.com>
2017-12-06 20:53:34 -0500

    hw49

--- v2/Protagonist.java
+++ v2/Protagonist.java
@@ -3,26 +3,26 @@ public class Protagonist extends Character {
     protected String name;
     
     public Protagonist (String username) {
-	name = username;
-	health = 125;
-	strength = 100;
-	defense = 40;
-	attack = 0.4;
+		name = username;
+		health = 125;
+		strength = 100;
+		defense = 40;
+		attack = 0.4;
     }
 
-    public String getName () {
-	return name;
+    public String getName() {
+		return name;
     }
 
     public void specialize () {
-	int boost =  1 + (int) (Math.random() * defense);
-	defense -= boost;
-	attack += boost;
+		int boost =  1 + (int) (Math.random() * defense);
+		defense -= boost;
+		attack += boost;
     }
 
     public void normalize () {
-	defense = 40;
-	attack = 0.4;
+		defense = 40;
+		attack = 0.4;
     }
 
     public String toString(){
